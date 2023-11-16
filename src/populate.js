d169d9b146899d34f0ff2d1359a44300453b5e71
GichukiMwangi88 <robertmwangi732@gmail.com>
2023-11-16 16:10:24 -0700

    App is complete. Need to figure out how to add customized icons based on weather conditions.

--- src/populate.js
+++ src/populate.js
@@ -1,8 +1,28 @@
 import getWeather from "./fetch.js";
 
+// Grab and create DOM elements
+// Get the weather card div
+const weatherCard = document.querySelector(".weatherCard");
+
+//Create DOM elements
+const icon = document.createElement("img");
+const location = document.createElement("h2");
+const date = document.createElement("p");
+const time = document.createElement("p");
+const temp = document.createElement("p");
+const conditions = document.createElement("p");
+
+// Set attributes to allow styling
+icon.setAttribute("id", "conditionImg");
+location.setAttribute("id", "locationName");
+date.setAttribute("id", "date");
+time.setAttribute("id", "localtime");
+temp.setAttribute("id", "tempF");
+conditions.setAttribute("id", "conditions");
 // Create a weather object with the weather details from API call
 
 const weatherObject = {
+  icon: "",
   location: "",
   date: "",
   time: "",
@@ -17,7 +37,7 @@ export default async function weatherCreate() {
     // Check if input is empty
 
     if (!cityInput) {
-      console.error("City input element not found");
+      alert("City input element not found");
       return;
     }
 
@@ -26,13 +46,14 @@ export default async function weatherCreate() {
 
     //validate city input
     if (!city) {
-      console.error("Please enter a valid city!");
+      alert("Please enter a valid city!");
       return;
     }
 
     const weatherData = await getWeather(city);
-    console.log(weatherData);
+    //console.log(weatherData);
 
+    weatherObject.icon = weatherData.current.condition.icon;
     weatherObject.location = weatherData.location.name;
     weatherObject.date = new Date(
       weatherData.location.localtime
@@ -44,6 +65,22 @@ export default async function weatherCreate() {
     weatherObject.conditions = weatherData.current.condition.text;
 
     console.log(weatherObject);
+
+    //Populate the page
+    icon.src = weatherObject.icon;
+    location.textContent = weatherObject.location;
+    date.textContent = `Date: ${weatherObject.date}`;
+    time.textContent = `Time: ${weatherObject.time}`;
+    temp.textContent = `Temp: ${weatherObject.temp}F`;
+    conditions.textContent = `Conditions: ${weatherObject.conditions}`;
+
+    // Append the elements to the weatherCard div
+    weatherCard.appendChild(icon);
+    weatherCard.appendChild(location);
+    weatherCard.appendChild(date);
+    weatherCard.appendChild(time);
+    weatherCard.appendChild(temp);
+    weatherCard.appendChild(conditions);
   } catch (error) {
     console.error("Error fetching weather data:", error.message);
   }
